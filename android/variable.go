2ed518d33935ea61206b543c8fcecf4ac8ef1b6a
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195
    Signed-off-by: RahifM <faizel326@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -133,6 +133,10 @@ type variableProperties struct {
                         Cppflags []string
                 }
 
+		Target_shim_libs struct {
+			Cppflags []string
+		}
+
 	} `android:"arch_variant"`
 }
 
@@ -299,6 +303,7 @@ type productVariables struct {
 
 	Target_init_vendor_lib  *string `json:",omitempty"`
         Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Target_shim_libs *string `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
