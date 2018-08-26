c03482a2ab33061c7c28dd899d33b509f5d7596b
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-25 20:23:19 -0600

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17
    Signed-off-by: RahifM <rahifmanjatha372@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -140,6 +140,10 @@ type variableProperties struct {
 			Whole_static_libs []string
 		}
 
+                Additional_gralloc_10_usage_bits struct {
+                        Cppflags []string
+                }
+
 	} `android:"arch_variant"`
 }
 
@@ -348,6 +352,7 @@ type productVariables struct {
 
         Supports_extended_compress_format  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+        Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
